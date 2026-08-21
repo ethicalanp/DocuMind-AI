b1e243d5e592cd2f329efd700401190f0a3d2eb8
ethicalanp <ethicalanp66@gmail.com>
2026-08-21 16:43:44 +0530

    main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from app.core.database import engine, Base
+from app.models.user import User
+
+
+Base.metadata.create_all(bind=engine)
 
 app=FastAPI(
     title="DocuMind AI",
