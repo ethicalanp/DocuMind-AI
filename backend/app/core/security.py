1e67b750a222c8a7ebd0bb73b94ecbe3acf3dbf1
ethicalanp <ethicalanp66@gmail.com>
2026-08-28 18:24:45 +0530

    security

---
+++ backend/app/core/security.py
@@ -0,0 +1,39 @@
+from pwdlib import PasswordHash
+from datetime import datetime, timedelta, timezone
+import jwt
+from app.core.config import SECRET_KEY,ALGORITHM,ACCESS_TOKEN_EXPIRE_MINUTES
+
+
+password_hash = PasswordHash.recommended()
+
+
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
+
+
+def verify_password(
+    plain_password: str,
+    hashed_password: str
+) -> bool:
+    return password_hash.verify(
+        plain_password,
+        hashed_password
+    )
+
+def create_access_token(data: dict) -> str:
+
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({
+        "exp": expire
+    })
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
