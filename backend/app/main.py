c204532e82c49fec1e99006b60cb69c048c63294
ethicalanp <ethicalanp66@gmail.com>
2026-08-28 18:23:15 +0530

    run app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from app.core.database import engine, Base
 from app.models.user import User
+from app.api.auth import router as auth_router
+from app.models.document import Document
+from app.api.documents import router as documents_router
+from app.models.document_chunk import DocumentChunk
+from app.api.chat import router as chat_router
+from app.models.conversation import Conversation
+from app.models.message import Message
+from app.api.conversations import router as conversations_router
 
 
 Base.metadata.create_all(bind=engine)
@@ -11,7 +19,16 @@ app=FastAPI(
     version="1.0"
 )
 
+app.include_router(auth_router)
+app.include_router(auth_router)
+app.include_router(documents_router)
+app.include_router(chat_router)
+app.include_router(conversations_router)
+
+
 @app.get("/")
 def root():
     return{"message":"DocuMind AI API is running"}
+
+
 
